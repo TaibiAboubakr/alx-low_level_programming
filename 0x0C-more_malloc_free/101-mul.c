1558324aae1bd80ce92799d9569d668243b0cf00
aboubakr <c.aboubakr@gmail.com>
2023-07-13 22:47:25 +0100

    Project 0x0c more malloc and free Task 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,9 +4,73 @@
 #include <string.h>
 #include <errno.h>
 #include <limits.h>
+void check(int len_arg1, int len_arg2, char *av[]);
+int checkiszero(int l1, int l2, int *p1, int *p2);
+void forfree(int **pp, int *pn1, int *pn2, int *pt, int len_arg1);
+/**
+ * check - check if the passed arguments are correct.
+ * @len_arg1:len of number 1
+ * @len_arg2:len of number 2
+ * @av:pointer to the array that store arguments
+ *
+ */
+void check(int len_arg1, int len_arg2, char *av[])
+{
+int i = 0;
+for (i = 0; i < len_arg1; i++)
+if (av[1][i] < 48 || av[1][i] > 57)
+{
+printf("Error\n");
+exit(98);
+}
+for (i = 0; i < len_arg2; i++)
+if (av[2][i] < 48 || av[2][i] > 57)
+{
+printf("Error\n");
+exit(98);
+}
+}
+/**
+ * checkiszero - check if number1 or number 2 is equal 0.
+ * @l1:len of number 1
+ * @l2:len of number 2
+ * @p1:pointer to the array that store number 1
+ * @p2:pointer to the array that store number 2
+ * Return: 0 if mul of tow arguments passed is equal to 0 or 1 otherwise
+ */
+int checkiszero(int l1, int l2, int *p1, int *p2)
+{
+int i, sum1 = 0, sum2 = 0;
+for (i = 0; i < l1; i++)
+sum1 += p1[i];
+for (i = 0; i < l2; i++)
+sum2 += p2[i];
+if (sum1 == 0 || sum2 == 0)
+return (0);
+return (1);
+}
+/**
+ * forfree - free all used pointers
+ * @pp: The number of command-line arguments
+ * @pn1:Poninter
+ * @pn2 : Poninter
+ * @pp : Poninter
+ * @pt : Poninter
+ * @len_arg1:len of number 1
+ */
+void forfree(int **pp, int *pn1, int *pn2, int *pt, int len_arg1)
+{
+int i;
+for (i = 0; i < len_arg1; i++)
+free(pp[i]);
+free(pn1);
+free(pn2);
+free(pt);
+free(pp);
+}
 
 /**
- * main -  a program that multiplies two positive numbers.
+ * main - program that multiplies two positive numbers.
  * @argc: The number of command-line arguments
  * @argv: An array of strings representing the arguments
  * num1 and num2 should only be composed of digits
@@ -14,78 +78,113 @@
  * Print the result, followed by a new line
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
-int *pn1, *pn2, *pnres;
-char* arg1 = argv[1], *arg2 = argv[2];
-int len_arg1,len_arg2,i,m,r = 0;
+int *pn1, *pn2, **pp, *pt;
+char *arg1 = argv[1], *arg2 = argv[2];
+int len_arg1, len_arg2, i, j = 0, m, l = 0, r = 0, idxn1 = 0, k, tsum = 0;
 if (argc < 3)
 {
 printf("Error\n");
-exit (98);
+exit(98);
 }
-len_arg1=strlen(argv[1]);
-len_arg2=strlen(argv[2]);
-for (i = 0; i < len_arg1; i++)
-if (argv[1][i] < 48 || argv[1][i] > 57)
-{
-printf("Error\n");
-exit (98);
-}
-for (i = 0; i < len_arg2; i++)
-if (argv[2][i] < 48 || argv[2][i] > 57)
-{
-printf("Error\n");
-exit (98);
-}
+len_arg1 = strlen(argv[1]);
+len_arg2 = strlen(argv[2]);
+check(len_arg1, len_arg2, argv);
 pn1 = (int *)malloc(len_arg1 *sizeof(int));
-if (pn1 == NULL){
-exit (98);
+if (pn1 == NULL)
+{
+printf("Error\n");
+exit(98);
 }
-pn2 = (int *)malloc(len_arg1 *sizeof(int));
-if (pn2 == NULL){
+pn2 = (int *)malloc(len_arg2 *sizeof(int));
+if (pn2 == NULL)
+{
+printf("Error\n");
 free(pn1);
-exit (98);
+exit(98);
 }
-for (i = 0; i < len_arg1; i++){
+for (i = 0; i < len_arg1; i++)
 pn1[i] = arg1[i] - 48;
-/* printf("in ptr n1 %d\n",pn1[i]); */
+
+for (i = 0; i < len_arg2; i++)
+pn2[i] = arg2[i] - 48;
+
+if (checkiszero(len_arg1, len_arg2, pn1, pn2) == 0)
+{
+printf("0\n");
+return (0);
 }
-for (i = 0; i < len_arg2; i++){
-/* printf("arv2 is : %c\n",argv[2][i]); */
-pn2[i]=arg2[i] - 48;
-/* printf("in ptr n2 %d\n",pn2[i]); */
+pp = (int **)malloc(len_arg1 *sizeof(int *));
+if (pp == NULL)
+{
+printf("Error\n");
+free(pn1);
+free(pn2);
+exit(98);
 }
-pnres = (int *)malloc((len_arg1 + len_arg2) *sizeof(int));
-if (pnres == NULL){
-exit (98);
+for (i = 0; i < len_arg1; i++)
+{
+pp[i] = (int *)malloc((len_arg1 + len_arg2 + i) * sizeof(int));
+if (pp[i] == NULL)
+{
+while (j < i)
+free(pp[j]);
+free(pn1);
+free(pn2);
+free(pp);
+printf("Error\n");
+exit(91);
 }
-/* printf("after for calc\n"); */
-for (i = 0;;i++){
-if (len_arg1 >= 0 && len_arg2 >= 0){
-m = pn1[len_arg1-1] * pn2[len_arg2-1] + r;
+}
+idxn1 = (len_arg1 - 1);
+for (k = len_arg1 - 1, i =  0; i < len_arg1; i++, idxn1++, k--)
+{
+for (j = len_arg2 - 1, l = (len_arg1 + len_arg2 - 1); j >= 0 ; j--, l--)
+{
+m = pn1[k] * pn2[j] + r;
 r = m / 10;
-pnres[i] = m % 10;
+pp[i][l - i] = m % 10;
+if (j == 0 && r > 0)
+{
+l--;
+pp[i][l - i] = r;
+r = 0;
 }
-if (len_arg1 >= 0 && len_arg2 < 0){
-m = pn1[len_arg1-1] + r;
-r = m / 10;
-pnres[i] = m % 10;
 }
-if (len_arg2 >= 0 && len_arg1 < 0){
-m = pn2[len_arg2-1] + r;
-r = m / 10;
-pnres[i] = m % 10;
 }
-len_arg1--;
-len_arg2--;
-if (len_arg1 < 0 && len_arg2 < 0 && r > 0){
-  i++;
-  pnres[i] = r;
-    r = 0;
+
+k = len_arg1 + len_arg2 + 1;
+pt = (int *)malloc(k *sizeof(int));
+if (pt == NULL)
+{
+for (i = 0; i < len_arg1; i++)
+free(pp[i]);
+free(pn1);
+free(pn2);
+free(pp);
+exit(98);
 }
-if (len_arg1 < 0 && len_arg2 < 0 && r == 0)
-break;
+for (i = len_arg1 + len_arg2; i >= 0; i--)
+{
+tsum = 0;
+for (j = 0; j < len_arg1; j++)
+{
+tsum += pp[j][i];
 }
+tsum = tsum + r;
+r = tsum / 10;
+pt[k] = tsum % 10;
+k--;
+}
+
+i = 0;
+while (pt[i] == 0)
+i++;
+for (; i <= len_arg1 + len_arg2; i++)
+printf("%d", pt[i]);
+printf("\n");
+forfree(pp, pn1, pn2, pt, len_arg1);
 return (0);
 }
