16f9d3fc00df4ed2a4c0b2547453405670fb9412
aboubakr <c.aboubakr@gmail.com>
2023-07-17 13:22:41 +0100

    Project 0x0E-structures_typedef Edit file :  1-init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
+if (d == NULL)
+exit(0);
 if (name != NULL)
 d->name = strdup(name);
 if (owner != NULL)
@@ -20,4 +21,3 @@ d->owner = strdup(owner);
 d->age = age;
 
 }
- 
