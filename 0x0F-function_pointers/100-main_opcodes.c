0a66722898f8ecde592fdf13b157cabdff2ae98d
aboubakr <c.aboubakr@gmail.com>
2023-07-19 14:36:56 +0100

    Project 0x0F-function_pointers Edit Task advanced

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "3-calc.h"
 
+void print_opcodes(int num_bytes);
 
+/**
+ * print_opcodes - function that print opcodes for main function
+ * @num_bytes : number of bytes to be printed.
+ */
+void print_opcodes(int num_bytes)
+{
+int i;
+unsigned char *opcode = (unsigned char *)print_opcodes;
+/* printf("The opcodes of the main function:\n"); */
+for (i = 0; i < num_bytes; i++)
+printf("%02x ", opcode[i]);
+printf("\n");
+}
 
 /**
  * main - check the code
@@ -13,26 +26,29 @@
  */
 int main(int argc, char *argv[])
 {
-int res, i, j, x, neg = 0;
+
+int res, i, x, neg = 0;
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-
 for (i = 0; argv[1][i]; i++)
 {
-if (argv[1][0] == '-'){
+if (argv[1][0] == '-')
+{
 neg = 1;
 continue;
 }
-if (argv[1][i] < 48 || argv[1][i] > 57){
+if (argv[1][i] < 48 || argv[1][i] > 57)
 exit(1);
 }
-}
-if (neg == 1){
+if (neg == 1)
+{
 printf("Error\n");
 exit(2);
 }
+x = atoi(argv[1]);
+print_opcodes(x);
 return (0);
 }
