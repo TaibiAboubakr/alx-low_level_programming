43d2751b5d4eb3a7dd1961c10f09ac64f71f8bbd
aboubakr <c.aboubakr@gmail.com>
2023-07-20 11:04:03 +0100

    Project 0x10-variadic_functions file 3-print all : update condition

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,31 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
-
+/**
+ * print_all - function that prints anythng, followed by a new line.
+ * @format: is a list of types of arguments passed to the function
+ * Return: numbers passed to function .
+ */
+/* int (*get_datatype(char *s))(int, int) */
+/* {
+op_t type[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+i = 0;
+while (ops[i].op != NULL)
+{
+if (strcmp(ops[i].op, s) == 0)
+return (ops[i].f);
+i++;
+}
+printf("Error : No Operator Matched\n");
+return (NULL);
+} */
 /**
  * print_all - function that prints anythng, followed by a new line.
  * @format: is a list of types of arguments passed to the function
@@ -8,6 +33,7 @@
  */
 void print_all(const char * const format, ...)
 {
+int count = 0, check;
 int i;
 char *s;
 float f;
@@ -17,31 +43,37 @@ ptr = format;
 va_start(args, format);
 while (*ptr)
 {
+check = 1;
 switch (*ptr)
 {
 case 'i':
+count++;
 i =  va_arg(args, int);
 printf("%d", i);
 break;
 case 'c':
+count++;
 i =  va_arg(args, int);
 printf("%c", i);
 break;
 case 'f':
+count++;
 f =  va_arg(args, double);
 printf("%f\n", f);
 break;
 case 's':
+count++;
 s =  va_arg(args, char *);
 if (s == NULL)
 s = "(nil)";
 printf("%s", s);
 break;
 default:
+check = 0;
 break;
 }
 ptr++;
-if (*ptr)
+if (*ptr && count > 0 && check !=0)
 printf(", ");
 }
 va_end(args);
