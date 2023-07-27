5b080cc7413a0f489014a2b74808a81583d8562c
aboubakr <c.aboubakr@gmail.com>
2023-07-27 12:58:27 +0100

    Project : 0x12-singly_linked_lists, add file 100-first.c (function that executed before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first -  function that prints message before the main function is executed.
+ */
+
+
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
