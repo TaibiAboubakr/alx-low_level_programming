578596dd711fb9f77a684a3e9578c5e990a41ba7
aboubakr <c.aboubakr@gmail.com>
2023-08-07 12:26:06 +0100

    project 0x15 file 2-append_text_to_file : fix output in case that text_content == NULL by adding if condition and edit betty doc and betty style

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,10 +2,10 @@
 
 
 /**
- * create_file - Write a function that appends text at the end of a file.
+ * append_text_to_file - function that appends text at the end of a file.
  * @filename: pointer to string that contain the name of the file
  * @text_content: is a NULL terminated string to add at the end of the file
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -19,13 +19,18 @@ return (-1);
 o = open(filename, O_WRONLY | O_APPEND);
 if (o == -1)
 return (-1);
+
+if (text_content == NULL)
+text_content = "\0";
 while (text_content[i])
 i++;
 
 w = write(o, text_content, i);
 if (w == -1)
+{
+close(o);
 return (0);
-
+}
 close(o);
 
 return (1);
