d6d3b9a995b282283399d87954ac8e2e8ae5c1b7
aboubakr <c.aboubakr@gmail.com>
2023-08-07 12:09:13 +0100

    Edit betty for main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void closefiles (ssize_t of_src, ssize_t of_dst);
-void closefile (ssize_t of_src);
+void closefiles(ssize_t of_src, ssize_t of_dst);
+void closefile(ssize_t of_src);
 #endif /* MAIN_H */
