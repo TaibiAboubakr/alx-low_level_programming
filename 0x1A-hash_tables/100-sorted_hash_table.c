d6e22d224c4ef1f10ccc7c99e14109d97ee75a07
aboubakr <c.aboubakr@gmail.com>
2023-10-06 10:44:42 +0100

    Project 0x1A-hash_tables advanced task 100-sorted hash table and hash_tables.h

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -171,30 +171,3 @@ void shash_table_print_rev(const shash_table_t *ht)
 	}
 	printf("}\n");
 }
-
-/**
- * shash_table_delete -  function that deletes a sorted hash table.
- * @ht:  is the hash table you want to look into
- */
-void shash_table_delete(shash_table_t *ht)
-{
-	unsigned long int idx = 0;
-	shash_node_t *temp;
-
-	if (!ht)
-		return;
-	for (idx = 0; idx < ht->size; idx++)
-	{
-		temp = ht->array[idx];
-		while (temp)
-		{
-			free(temp->key);
-			free(temp->value);
-			ht->array[idx] = temp->next;
-			free(temp);
-			temp = ht->array[idx];
-		}
-	}
-	free(ht->array);
-	free(ht);
-}
