454ea7675541629e560c21bb20c0d5a197bdce3c
aboubakr <c.aboubakr@gmail.com>
2024-01-15 17:49:50 +0100

    update the file search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int searching(int *array, int value, int low, int high);
+int jump_search(int *array, size_t size, int value);
 
 
 #endif /* SEARCH_ALGOS */
