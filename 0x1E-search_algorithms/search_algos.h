727d1c6ffd0372e03b042efdcff5f0834b601506
aboubakr <c.aboubakr@gmail.com>
2024-01-17 21:05:22 +0100

    update the file search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,6 +12,10 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, int value, size_t low, size_t high);
+int exponential_search(int *array, size_t size, int value);
+int binary_search_1(int *array, int start, int end, int value);
+
+
 
 
 
